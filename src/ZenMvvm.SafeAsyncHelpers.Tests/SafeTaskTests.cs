333984db296d3886feb8ee913d153fb700c06148
Zeebs <zeebsmail@gmail.com>
2020-12-17 12:52:03 +0200

    Improve readability of SafeTaskTests

--- src/ZenMvvm.SafeAsyncHelpers.Tests/SafeTaskTests.cs
+++ src/ZenMvvm.SafeAsyncHelpers.Tests/SafeTaskTests.cs
@@ -9,29 +9,40 @@ using Zeebs.UnitTestHelpers;
 namespace ZenMvvm.Tests
 {
     [Collection("SafeTests")]
-    public class SafeTaskTests
+    public class SafeTaskTests : IDisposable
     {
+        private readonly Mock<ISafeExecutionHelpers> mockHelpers;
+        private readonly SpecificException specificException = new();
+        private readonly SafeTask sut = new();
+        private readonly DeterministicTaskScheduler dts = new(shouldThrowExceptions: false);
+
+        //Setup
+        public SafeTaskTests()
+        {
+            mockHelpers = new Mock<ISafeExecutionHelpers>();
+            SafeExecutionHelpers.Implementation = mockHelpers.Object;
+        }
+
+        //Teardown
+#pragma warning disable CA1816 // Dispose methods should call SuppressFinalize
+        public void Dispose()
+#pragma warning restore CA1816 // Dispose methods should call SuppressFinalize
+        {
+            SafeExecutionHelpers.RevertToDefaultImplementation();
+        }
+
         class SpecificException : Exception { }
 
         [Fact]
         public void SafeContinueWith_NothingSet_HandleExceptionRuns()
         {
-            SpecificException specificException = new SpecificException();
-
-            SafeExecutionHelpers.RevertToDefaultImplementation();
-            var mockHelpers = new Mock<ISafeExecutionHelpers>();
-            SafeExecutionHelpers.Implementation = mockHelpers.Object;
-
-            var dts = new DeterministicTaskScheduler(shouldThrowExceptions: false);
-            var sut = new SafeTask();
-
             sut.SafeContinueWith<Exception>(
                 Task.Factory.StartNew(
                     () => throw specificException,
                     CancellationToken.None,
                     TaskCreationOptions.None,
                     dts),
-                null,
+                onException: null,
                 dts);
 
             dts.RunTasksUntilIdle();
@@ -45,15 +56,7 @@ namespace ZenMvvm.Tests
         [Fact]
         public void SafeContinueWith_OnExceptionExceptionSet_HandlesException()
         {
-            SpecificException specificException = new SpecificException();
             Action<Exception> onException = new Mock<Action<Exception>>().Object;
-
-            SafeExecutionHelpers.RevertToDefaultImplementation();
-            var mockHelpers = new Mock<ISafeExecutionHelpers>();
-            SafeExecutionHelpers.Implementation = mockHelpers.Object;
-
-            var dts = new DeterministicTaskScheduler(shouldThrowExceptions: false);
-            var sut = new SafeTask();
 
             sut.SafeContinueWith<Exception>(
                 Task.Factory.StartNew(
@@ -68,134 +71,31 @@ namespace ZenMvvm.Tests
 
             Assert.Contains(specificException, dts.Exceptions);
             mockHelpers.Verify(h => h.HandleException(specificException, onException));
-
-            SafeExecutionHelpers.RevertToDefaultImplementation();
         }
 
         [Fact]
         public void SafeContinueWith_DefaultExceptionHandlerSet_HandlesException()
         {
-            SpecificException specificException = new SpecificException();
-            Action<Exception> defaultExceptionHandler = new Mock<Action<Exception>>().Object;
-
-            SafeExecutionHelpers.RevertToDefaultImplementation();
-            var mockHelpers = new Mock<ISafeExecutionHelpers>();
-
+            Action<Exception> defaultExceptionHandler
+                = new Mock<Action<Exception>>().Object;
+            
             //Crux - DefaultHandler returns non-null delegate
             mockHelpers.SetupGet(h => h.Settings.DefaultExceptionHandler).Returns(defaultExceptionHandler);
 
-            SafeExecutionHelpers.Implementation = mockHelpers.Object;
-
-            var dts = new DeterministicTaskScheduler(shouldThrowExceptions: false);
-            var sut = new SafeTask();
-
             sut.SafeContinueWith<Exception>(
-                Task.Factory.StartNew(
+                task: Task.Factory.StartNew(
                     () => throw specificException,
                     CancellationToken.None,
                     TaskCreationOptions.None,
                     dts),
-                null,
-                dts);
+                onException: null,
+                scheduler: dts);
 
             dts.RunTasksUntilIdle();
 
             Assert.Contains(specificException, dts.Exceptions);
-            mockHelpers.Verify(h => h.HandleException<Exception>(specificException,null));
-
-            SafeExecutionHelpers.Implementation = mockHelpers.Object;
+            mockHelpers.Verify(h =>
+                h.HandleException<Exception>(specificException, null));
         }
-
-
-        //Tested more directly
-        //[Fact]
-        //public void SafeContinueWith_Exception_TargetThrowsException_IsHandled()
-        //{
-        //    bool isHandled = false;
-        //    SpecificException exception = new SpecificException();
-
-        //    Task ThrowingAsync()
-        //    { throw exception; }
-
-        //    var dts = new DeterministicTaskScheduler(shouldThrowExceptions: false);
-        //    var sut = new SafeTask();
-
-        //    sut.SafeContinueWith<Exception>(
-        //        Task.Factory.StartNew(
-        //            ThrowingAsync,
-        //            CancellationToken.None,
-        //            TaskCreationOptions.None,
-        //            dts),
-        //        (ex) => isHandled = true,dts);
-
-        //    dts.RunTasksUntilIdle();
-
-        //    Assert.Contains(exception, dts.Exceptions);
-        //    Assert.True(isHandled);
-        //}
-
-        //Tested more directly
-        //[Fact]
-        //public void SafeContinueWith_SpecificException_TargetThrowsSpecificException_IsHandled()
-        //{
-        //    bool isHandled = false;
-        //    SpecificException exception = new SpecificException();
-
-        //    Task ThrowingAsync()
-        //    {  throw exception; }
-
-        //    var dts = new DeterministicTaskScheduler(shouldThrowExceptions: false);
-        //    Task.Factory.StartNew(
-        //    ThrowingAsync, CancellationToken.None, TaskCreationOptions.None, dts).SafeContinueWith<SpecificException>((ex) => isHandled = true, dts);
-        //    dts.RunTasksUntilIdle();
-
-        //    Assert.Contains(exception, dts.Exceptions);
-        //    Assert.True(isHandled);
-
-        //}
-
-        //Tested more directly
-        //[Fact]
-        //public void SafeContinueWith_Exception_TargetThrowsSpecificException_IsHandled()
-        //{
-        //    SafeExecutionHelpers.Initialize();
-        //    SafeExecutionHelpers.RemoveDefaultExceptionHandler();
-
-        //    bool isHandled = false;
-        //    SpecificException exception = new SpecificException();
-
-        //    Task ThrowingAsync()
-        //    { throw exception; }
-
-        //    var dts = new DeterministicTaskScheduler(shouldThrowExceptions: false);
-        //    Task.Factory.StartNew(
-        //    ThrowingAsync, CancellationToken.None, TaskCreationOptions.None, dts).SafeContinueWith<Exception>((ex) => isHandled = true, dts);
-        //    dts.RunTasksUntilIdle();
-
-        //    Assert.Contains(exception, dts.Exceptions);
-        //    Assert.True(isHandled);
-
-        //}
-
-
-        //Tested more directly
-        //[Fact]
-        //public void SafeContinueWith_SpecificException_TargetThrowsDifferentException_NotHandled()
-        //{
-        //    bool isHandled = false;
-        //    DifferentException exception = new DifferentException();
-
-        //    Task ThrowingAsync()
-        //    { throw exception; }
-
-        //    var dts = new DeterministicTaskScheduler(shouldThrowExceptions: false);
-        //    Task.Factory.StartNew(
-        //    ThrowingAsync, CancellationToken.None, TaskCreationOptions.None, dts).SafeContinueWith<SpecificException>((ex) => isHandled = true, dts);
-        //    dts.RunTasksUntilIdle();
-
-        //    Assert.Contains(exception, dts.Exceptions);
-        //    Assert.False(isHandled);
-
-        //}
     }
 }
